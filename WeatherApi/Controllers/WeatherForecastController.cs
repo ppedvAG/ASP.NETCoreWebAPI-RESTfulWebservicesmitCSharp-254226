922044adaba3d855a15eb61c0822f3ad4072cfd4
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-08-04 12:00:38 +0200

    Module 001 Einstieg

---
+++ WeatherApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using WeatherApi.Models;
+using WeatherApi.Services;
+
+namespace WeatherApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var service = new WeatherForecastService();
+        var result = service.GetWeatherForecasts();
+        _logger.LogInformation("Es wurden Wetterdaten fuer 5 Tage geladen.");
+        return result;
+    }
+}
---
+++ WeatherApi/Services/WeatherForecastService.cs
@@ -0,0 +1,21 @@
+using WeatherApi.Models;
+
+namespace WeatherApi.Services;
+
+public class WeatherForecastService
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public IEnumerable<WeatherForecast> GetWeatherForecasts()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
+    }
+}
