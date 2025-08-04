ed755c8d101ae05658eb3dfcc4288aed54941cb8
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-08-04 13:12:30 +0200

    Lab 002 Loesung

---
+++ LabDependencyInjection/Program.cs
@@ -0,0 +1,22 @@
+using Lab002_DependencyInjection.Services;
+
+namespace LabDependencyInjection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSingleton<IOperationSingleton, OperationService>();
+            builder.Services.AddScoped<IOperationScoped, OperationService>();
+            builder.Services.AddTransient<IOperationTransient, OperationService>();
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.MapControllerRoute("default", "{controller=Test}");
+
+            app.Run();
+        }
+    }
+}
---
+++ LabDependencyInjection/Services/OperationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab002_DependencyInjection.Services
+{
+    public class OperationService : IOperationScoped, IOperationSingleton, IOperationTransient
+    {
+        // [^4..] - Kurze Schreibweise, um die letzten 4 Zeichen von der Guid zu erhalten
+        public string OperationId { get; } = Guid.NewGuid().ToString()[^4..];
+
+    }
+}
